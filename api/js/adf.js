045631cc4515a6bd78263648b8f4f3485f851961
Matteo Nicoli <matteo.nicoli001@gmail.com>
2024-08-20 01:17:53 +0200

    wip

---
+++ api/js/adf.js
@@ -0,0 +1,45 @@
+/* adf.js - Implementation of the JS interface for the ADF library
+ * ------------------------------------------------------------------------
+ * ADF - Agriculture Data Format
+ * Copyright (C) 2024 Matteo Nicoli
+ *
+ * This file is part of Terius
+ *
+ * ADF is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Terius is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+const fs = require('fs');
+var source = fs.readFileSync('./adflib.wasm');
+const env = {
+    memoryBase: 0,
+    tableBase: 0,
+    memory: new WebAssembly.Memory({
+        initial: 256
+    }),
+    table: new WebAssembly.Table({
+        initial: 0,
+        element: 'anyfunc'
+    })
+};
+
+var typedArray = new Uint8Array(source);
+
+WebAssembly.instantiate(typedArray, {
+    env: env
+}).then(result => {
+    console.log(result.instance.exports.get_version_bytes());
+}).catch(e => {
+    console.log(e);
+});
